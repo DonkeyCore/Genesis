d75439e951335f4883e55e35ca587be138c266a1
DonkeyTeeth2013 <doomsdaysurvivor2013@gmail.com>
2015-07-24 10:40:31 -0500

    Various bugfixes and improvements #2

--- Genesis/src/genesis/FileManager.java
+++ Genesis/src/genesis/FileManager.java
@@ -1,12 +1,16 @@
 package genesis;
 
+import genesis.Genesis.PrintWriterStream;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 
@@ -30,12 +34,12 @@ public final class FileManager {
 	
 	private void checkFiles() {
 		try {
-			if (!log.exists())
+			if(!log.exists())
 				log.createNewFile();
-			if (!resp.exists())
+			if(!resp.exists())
 				resp.createNewFile();
-		} catch (Throwable t) {
-			g.printError(t);
+		} catch(IOException e) {
+			g.printError(e);
 		}
 	}
 	
@@ -57,18 +61,18 @@ public final class FileManager {
 	
 	public HashMap<ResponseType, List<String>> getResponses() {
 		checkFiles();
-		if (resp.length() == 0)
-			return null;
 		HashMap<ResponseType, List<String>> res = new HashMap<ResponseType, List<String>>();
-		try (BufferedReader r = new BufferedReader(new FileReader(resp))) {
+		if(resp.length() == 0)
+			return res;
+		try(BufferedReader r = new BufferedReader(new FileReader(resp))) {
 			String line;
-			while ((line = r.readLine()) != null) {
-				for (ResponseType rt : ResponseType.values()) {
-					if (line.split("�")[0].equalsIgnoreCase(rt.name())) {
+			while((line = r.readLine()) != null) {
+				for(ResponseType rt : ResponseType.values()) {
+					if(line.split("�")[0].equalsIgnoreCase(rt.name())) {
 						String response = "";
-						for (int i = 1; i < line.split("�").length; i++)
+						for(int i = 1; i < line.split("�").length; i++)
 							response = line.split("�")[i].trim();
-						if (res.get(rt) == null) {
+						if(res.get(rt) == null) {
 							List<String> list = new ArrayList<String>();
 							list.add(response);
 							res.put(rt, list);
@@ -77,24 +81,23 @@ public final class FileManager {
 					}
 				}
 			}
-		} catch (Throwable t) {
-			g.printError(t);
+		} catch(IOException e) {
+			g.printError(e);
 		}
 		return res;
 	}
 	
 	public List<String> getResponses(ResponseType rt) {
-		checkFiles();
-		if (resp.length() == 0)
-			return null;
 		List<String> res = new ArrayList<String>();
-		try (BufferedReader r = new BufferedReader(new FileReader(resp))) {
+		if(resp.length() == 0)
+			return res;
+		try(BufferedReader r = new BufferedReader(new FileReader(resp))) {
 			String line;
-			while ((line = r.readLine()) != null) {
-				if (line.split("�")[0].equalsIgnoreCase(rt.name()))
+			while((line = r.readLine()) != null) {
+				if(line.split("�")[0].equalsIgnoreCase(rt.name()))
 					res.add(line.split("�")[1].trim());
 			}
-		} catch (Throwable t) {
+		} catch(Throwable t) {
 			g.printError(t);
 		}
 		return res;
@@ -102,38 +105,49 @@ public final class FileManager {
 	
 	public void setResponse(ResponseType type, String response) {
 		response = response.trim();
-		try (BufferedReader r = new BufferedReader(new FileReader(resp))) {
+		try(BufferedReader r = new BufferedReader(new FileReader(resp))) {
 			String s;
-			while ((s = r.readLine()) != null) {
-				if (s.equals(type.toString() + "� " + response))
+			while((s = r.readLine()) != null) {
+				if(s.equals(type.toString() + "� " + response))
 					return;
 			}
 			rout.println(type.toString() + "� " + response);
 			rout.flush();
-		} catch (Throwable t) {
+		} catch(Throwable t) {
 			g.printError(t);
 		}
 	}
 	
 	public void log(String message) {
 		checkFiles();
+		log(lout, message);
+	}
+	
+	public static void log(Object output, String message) {
+		PrintWriterStream out;
+		if(output instanceof PrintWriter)
+			out = new PrintWriterStream((PrintWriter) output);
+		else if(output instanceof PrintStream)
+			out = new PrintWriterStream((PrintStream) output);
+		else
+			throw new IllegalArgumentException("Output must be of type PrintWriter or PrintStream");
+		Calendar c = Calendar.getInstance();
+		String hour = c.get(Calendar.HOUR) + "";
+		String minute = c.get(Calendar.MINUTE) + "";
+		String second = c.get(Calendar.SECOND) + "";
+		int ampm = c.get(Calendar.AM_PM);
 		
-		String hour = java.util.Calendar.getInstance().get(java.util.Calendar.HOUR) + "";
-		String minute = java.util.Calendar.getInstance().get(java.util.Calendar.MINUTE) + "";
-		String second = java.util.Calendar.getInstance().get(java.util.Calendar.SECOND) + "";
-		int ampm = java.util.Calendar.getInstance().get(java.util.Calendar.AM_PM);
-		
-		if (Integer.parseInt(hour) < 10)
+		if(Integer.parseInt(hour) < 10) //keep the hours minutes and seconds 2 digits
 			hour = "0" + hour;
-		if (Integer.parseInt(minute) < 10)
+		if(Integer.parseInt(minute) < 10)
 			minute = "0" + minute;
-		if (Integer.parseInt(second) < 10)
+		if(Integer.parseInt(second) < 10)
 			second = "0" + second;
 		
 		String timestamp = "[" + hour + ":" + minute + ":" + second + ":" + (ampm == 0 ? "AM" : "PM") + "]";
 		
-		lout.println(timestamp + ": " + message);
-		lout.flush();
+		out.println(timestamp + ": " + message);
+		out.flush();
 	}
 	
 	public PrintWriter getLogStream() {
--- Genesis/src/genesis/Genesis.java
+++ Genesis/src/genesis/Genesis.java
@@ -1,6 +1,7 @@
 package genesis;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.io.PrintWriter;
@@ -15,15 +16,15 @@ public final class Genesis {
 	public static void main(String[] args) {
 		try {
 			new Genesis();
-		} catch(Throwable t) {
-			printError(new PrintWriter(System.err), t);
+		} catch(IOException e) {
+			printError(new PrintWriter(System.err), e);
 		}
 	}
 	
 	private final FileManager fm;
 	protected String last = null;
 	
-	public Genesis() throws Throwable {
+	public Genesis() throws IOException {
 		log("Initializing " + toString() + "...");
 		log("Generating files...");
 		fm = new FileManager(this);
@@ -37,11 +38,11 @@ public final class Genesis {
 	}
 	
 	public void stop(int error) {
-		if (error == 0)
+		if(error == 0)
 			log(toString() + " shut down successfully!");
 		else
 			log(toString() + " shut down with error code: " + error);
-		if (fm != null) {
+		if(fm != null) {
 			fm.getLogStream().println();
 			fm.close();
 		}
@@ -52,7 +53,7 @@ public final class Genesis {
 		try(BufferedReader r = new BufferedReader(new InputStreamReader(System.in))) {
 			System.out.print("You: ");
 			String s = r.readLine();
-			if (respond(s))
+			if(respond(s))
 				start();
 		} catch(Throwable t) {
 			printError(t);
@@ -60,83 +61,82 @@ public final class Genesis {
 	}
 	
 	public boolean respond(String s) {
-		if (s.trim().equals(""))
+		if(s.trim().equals(""))
 			return true;
 		String response = "";
-		if (last == null) {
-			if (s.trim().equalsIgnoreCase("exit"))
+		if(last == null) {
+			if(s.trim().equalsIgnoreCase("exit"))
 				return false;
 			boolean newg = true;
-			try {
-				for(String r : fm.getResponses(ResponseType.GREETING)) {
-					if (strip(s).equalsIgnoreCase(strip(r)))
-						newg = false;
-				}
-			} catch(Throwable t) {}
-			if (newg)
+			for(String r : fm.getResponses(ResponseType.GREETING)) {
+				if(transform(s).equalsIgnoreCase(transform(r)))
+					newg = false;
+			}
+			if(newg)
 				fm.setResponse(ResponseType.GREETING, removeEndPunc(s));
 			System.out.println(response = (name + ": " + format(fm.getResponses(ResponseType.GREETING).get((int) (System.nanoTime() % fm.getResponses(ResponseType.GREETING).size())))));
 		} else {
+			boolean notg = true;
+			for(String r : fm.getResponses(ResponseType.GREETING)) { //check if THE LAST MESSAGE is another greeting
+				if(transform(last).equalsIgnoreCase(transform(r)))
+					notg = false;
+			}
 			boolean notf = true;
-			try {
-				for(String r : fm.getResponses(ResponseType.FAREWELL)) {
-					if (strip(s).equalsIgnoreCase(strip(r)))
-						notf = false;
+			for(String r : fm.getResponses(ResponseType.FAREWELL)) {
+				if(transform(s).equalsIgnoreCase(transform(r)))
+					notf = false;
+			}
+			if((!notf || s.equalsIgnoreCase("exit")) && notg) {
+				boolean newf = true;
+				for(String r : fm.getResponses(ResponseType.FAREWELL)) { //check if it's a new farewell
+					if(transform(last).equalsIgnoreCase(transform(r)))
+						newf = false;
 				}
-			} catch(Throwable t) {}
-			if (!notf || s.equalsIgnoreCase("exit")) {
-				if (!s.equalsIgnoreCase("exit"))
+				if(newf) //if it's new, store it for another session (or this one)
 					fm.setResponse(ResponseType.FAREWELL, removeEndPunc(last));
+				//say bye back
 				System.out.println(response = (name + ": " + format(fm.getResponses(ResponseType.FAREWELL).get((int) (System.nanoTime() % fm.getResponses(ResponseType.FAREWELL).size())))));
-				return false;
+				return false; //exit the program
 			}
 		}
 		boolean containsLaugh = false;
 		for(String r : fm.getResponses(ResponseType.LAUGH)) {
-			if (s.matches(".*?\\b" + r + "\\b.*?"))
+			if(s.matches(".*?\\b" + r + "\\b.*?"))
 				containsLaugh = true;
 		}
 		boolean laughIfPossible = false;
 		int laugh = 0;
 		for(char c : s.toCharArray()) {
-			if (c == 'h' || c == 'l')
+			if(c == 'h' || c == 'l')
 				laugh++;
 		}
-		if (laugh > s.toCharArray().length / 2 && !fm.getResponses(ResponseType.GREETING).stream().anyMatch((g) -> {
-			return g.trim().equalsIgnoreCase(s.trim());
+		if(laugh > s.toCharArray().length / 2 && !fm.getResponses(ResponseType.GREETING).stream().anyMatch((g) -> {
+			return transform(g).equalsIgnoreCase(transform(s));
 		})) {
-			try {
-				boolean newl = true;
-				try {
-					for(String r : fm.getResponses(ResponseType.LAUGH)) {
-						if (strip(s).equalsIgnoreCase(strip(r)))
-							newl = false;
-					}
-				} catch(Throwable t) {}
-				if (newl)
-					fm.setResponse(ResponseType.LAUGH, removeEndPunc(s));
-				laughIfPossible = true;
-			} catch(Throwable t) {
-				printError(t);
+			boolean newl = true;
+			for(String r : fm.getResponses(ResponseType.LAUGH)) {
+				if(transform(s).equalsIgnoreCase(transform(r)))
+					newl = false;
 			}
+			if(newl)
+				fm.setResponse(ResponseType.LAUGH, removeEndPunc(s));
+			laughIfPossible = true;
 		}
-		if (!containsLaugh) {
+		if(!containsLaugh) {
 			String[] set = s.split("(\\s+is\\s+|\\'s\\s+)");
-			try {
-				System.out.println(response = (name + ": " + solve(strip(set[1]).trim())));
-			} catch(Throwable t) {
-				String ek = strip(set[0]);
-				if (ek.contains("what")) {
-					String k = strip(reversePerson(join(set, "is", 1)));
-					try {
-						for(String values : fm.getResponses(ResponseType.VALUE)) {
-							if (strip(values.split("§=§")[0]).trim().equalsIgnoreCase(k))
-								response = name + ": " + cap(k) + " is " + values.split("§=§")[1].trim() + punc();
-						}
-						if (!response.equals(""))
-							System.out.println(response);
-					} catch(Throwable t2) {}
-				} else if (s.contains(" is ")) {
+			try { //if it's math, solve it
+				System.out.println(response = (name + ": " + solve(transform(set[1]).trim())));
+			} catch(Throwable t) { //it's not math
+				String ek = transform(set[0]);
+				if(ek.contains("what")) {
+					String k = transform(reversePerson(join(set, "is", 1)));
+					for(String values : fm.getResponses(ResponseType.VALUE)) {
+						if(transform(values.split("§=§")[0]).trim().equalsIgnoreCase(k))
+							response = name + ": " + cap(k) + " is " + values.split("§=§")[1].trim() + punc();
+					}
+					if(!response.equals(""))
+						System.out.println(response);
+				} else if(s.contains(" is ")) {
 					String k = reversePerson(s.split(" is ")[0].trim());
 					String v = join(s.split("(?i) (is) "), "$1%s", 1).trim();
 					fm.setResponse(ResponseType.VALUE, k + "§=§" + reversePerson(removeEndPunc(v)));
@@ -144,7 +144,7 @@ public final class Genesis {
 				}
 			}
 		}
-		if (response.trim().equals("") && (laughIfPossible || containsLaugh))
+		if(response.trim().equals("") && (laughIfPossible || containsLaugh))
 			System.out.println(response = (name + ": " + cap(fm.getResponses(ResponseType.LAUGH).get(((int) (System.nanoTime() % fm.getResponses(ResponseType.LAUGH).size()))))));
 		fm.log("You: " + s);
 		fm.log(name + ": " + (response.replace(name + ": ", "")));
@@ -152,11 +152,11 @@ public final class Genesis {
 		return true;
 	}
 	
-	private static String join(String[] set, String medium, int start) {
-		String s = set[start];
+	private static String join(String[] set, String medium, int offset) {
+		String s = set[offset];
 		int i = 0;
 		for(String part : set) {
-			if (i > start)
+			if(i > offset)
 				s = s + " " + medium + " " + part;
 			i++;
 		}
@@ -170,13 +170,14 @@ public final class Genesis {
 	public static double solve(String c) {
 		Pattern p = Pattern.compile("(\\d+|\\d+\\.\\d+)\\s*(\\+|\\-|\\*|\\/|\\%|\\|)\\s*(\\d+|\\d+\\.\\d+).*");
 		Matcher m = p.matcher(c);
-		if (m.matches()) {
+		if(m.matches()) {
 			Double d1 = Double.parseDouble(m.group(1));
 			Double d2 = Double.parseDouble(m.group(3));
 			while(c.contains("+") || c.contains("-") || c.contains("*") || c.contains("/") || c.contains("%") || c.contains("|")) {
 				c = c.replaceAll("(\\d)\\.0(\\D)", "$1$2");
 				m = p.matcher(c);
-				m.matches();
+				if(!m.matches())
+					throw new ArithmeticException();
 				switch(m.group(2)) {
 					default:
 						break;
@@ -204,8 +205,8 @@ public final class Genesis {
 		return Double.parseDouble(c);
 	}
 	
-	private static String strip(String s) {
-		return s.replace("?", "").replace(".", "").replace("!", "").replace(",", "").replace("_", "").replace("~", "").replace("`", "").replace("'", "").replace("\"", "").replace("\"", "").replace("\\", "").replace(":", "").replace(";", "").replaceAll("(?i)the", " ").replaceAll("(?i)teh", " ").replaceAll("(?i)how\\s+do", "how can").replaceAll("(?i)re", "").replaceAll("(?i)\\s+a ", " ").replaceAll("(?i)is", "").replaceAll("(?i)has", "").replaceAll("(?i)get to", "go to").replaceAll("\\Bs\\b", "").replaceAll(" {2}?", "").trim();
+	private static String transform(String s) {
+		return s.replace("?", "").replace(".", "").replace("!", "").replace(",", "").replace("_", "").replace("~", "").replace("`", "").replace("'", "").replace("\"", "").replace("\\", "").replace(":", "").replace(";", "").replaceAll("(?i)the", " ").replaceAll("(?i)teh", " ").replaceAll("(?i)how\\s+do", "how can").replaceAll("(?i)re", "").replaceAll("(?i)\\s+a ", " ").replaceAll("(?i)is", "").replaceAll("(?i)has", "").replaceAll("(?i)get to", "go to").replaceAll("\\Bs\\b", "").replaceAll(" {2}?", "").trim();
 	}
 	
 	private static String removeEndPunc(String s) {
@@ -217,9 +218,10 @@ public final class Genesis {
 	}
 	
 	private static String cap(String s) {
-		if(s.length() < 1)
-			return s;
-		return s.replaceFirst(s.substring(0, 1), s.substring(0, 1).toUpperCase());
+		String r = s.toUpperCase();
+		if(s.length() > 1)
+			r = s.replaceFirst(s.substring(0, 1), s.substring(0, 1).toUpperCase());
+		return r;
 	}
 	
 	private static char punc() {
@@ -241,80 +243,52 @@ public final class Genesis {
 	
 	public void printError(Throwable t) {
 		printError(System.err, t);
-		if (fm != null)
+		if(fm != null)
 			printError(fm.getLogStream(), t);
 		stop(1);
 	}
 	
-	private static void printError(PrintStream out, Throwable t) {
+	private static void printError(Object output, Throwable t) {
+		PrintWriterStream out;
+		if(output instanceof PrintWriter)
+			out = new PrintWriterStream((PrintWriter) output);
+		else if(output instanceof PrintStream)
+			out = new PrintWriterStream((PrintStream) output);
+		else
+			throw new IllegalArgumentException("Output must be of type PrintWriter or PrintStream");
 		out.println();
 		out.println("A fatal error occurred: " + t.toString());
 		out.println();
 		out.println("-----=[General Stack Trace]=-----");
 		for(StackTraceElement s : t.getStackTrace())
+			//print the throwable's stack trace
 			out.println(s.getClassName() + "." + s.getMethodName() + "() on line " + s.getLineNumber());
 		out.println("-----=[" + name + " Stack Trace]=-----");
 		out.println();
 		out.println("-----=[" + name + " Stack Trace]=-----");
 		boolean fault = false;
-		for(StackTraceElement s : t.getStackTrace()) {
-			if (s.getClassName().startsWith("genesis")) {
+		for(StackTraceElement s : t.getStackTrace()) { //filter out the stack trace for only Genesis
+			if(s.getClassName().startsWith("genesis")) {
 				out.println(s.getClassName() + "." + s.getMethodName() + "() on line " + s.getLineNumber());
 				fault = true;
 			}
 		}
-		if (!fault)
+		if(!fault) //if it's not our fault, tell the user
 			out.println("This doesn't look like a problem relating to " + name + ". Check the above general stack trace.");
 		out.println("-----=[Genesis Stack Trace]=-----");
 		out.println();
 		out.println("-----=[Remote Stack Trace]=-----");
 		fault = false;
-		for(StackTraceElement s : t.getStackTrace()) {
-			if (!s.getClassName().startsWith("genesis")) {
+		for(StackTraceElement s : t.getStackTrace()) { //filter out the stack trace for only outside Genesis
+			if(!s.getClassName().startsWith("genesis")) {
 				out.println(s.getClassName() + "." + s.getMethodName() + "() on line " + s.getLineNumber());
 				fault = true;
 			}
 		}
-		if (!fault)
+		if(!fault) //if it's not their fault, tell the user
 			out.println("This doesn't look like a problem with anything outside " + name + ". Check the above " + name + " stack trace.");
 		out.println("-----=[Remote Stack Trace]=-----");
 		out.println();
-	}
-	
-	private static void printError(PrintWriter out, Throwable t) {
-		out.println();
-		out.println("A fatal error occurred: " + t.toString());
-		out.println();
-		out.println("-----=[General Stack Trace]=-----");
-		for(StackTraceElement s : t.getStackTrace())
-			out.println(s.getClassName() + "." + s.getMethodName() + "() on line " + s.getLineNumber());
-		out.println("-----=[General Stack Trace]=-----");
-		out.println();
-		out.println("-----=[" + name + " Stack Trace]=-----");
-		boolean fault = false;
-		for(StackTraceElement s : t.getStackTrace()) {
-			if (s.getClassName().startsWith("genesis")) {
-				out.println(s.getClassName() + "." + s.getMethodName() + "() on line " + s.getLineNumber());
-				fault = true;
-			}
-		}
-		if (!fault)
-			out.println("This doesn't look like a problem relating to " + name + ". Check the above general stack trace.");
-		out.println("-----=[" + name + " Stack Trace]=-----");
-		out.println();
-		out.println("-----=[Remote Stack Trace]=-----");
-		fault = false;
-		for(StackTraceElement s : t.getStackTrace()) {
-			if (!s.getClassName().startsWith("genesis")) {
-				out.println(s.getClassName() + "." + s.getMethodName() + "() on line " + s.getLineNumber());
-				fault = true;
-			}
-		}
-		if (!fault)
-			out.println("This doesn't look like a problem with anything outside " + name + ". Check the above " + name + " stack trace.");
-		out.println("-----=[Remote Stack Trace]=-----");
-		out.println();
-		out.flush();
 	}
 	
 	public void log(String message) {
@@ -322,23 +296,57 @@ public final class Genesis {
 	}
 	
 	public void log(PrintStream out, String message) {
-		String hour = java.util.Calendar.getInstance().get(java.util.Calendar.HOUR) + "";
-		String minute = java.util.Calendar.getInstance().get(java.util.Calendar.MINUTE) + "";
-		String second = java.util.Calendar.getInstance().get(java.util.Calendar.SECOND) + "";
-		int ampm = java.util.Calendar.getInstance().get(java.util.Calendar.AM_PM);
-		if (Integer.parseInt(hour) < 10)
-			hour = "0" + hour;
-		if (Integer.parseInt(minute) < 10)
-			minute = "0" + minute;
-		if (Integer.parseInt(second) < 10)
-			second = "0" + second;
-		String timestamp = "[" + hour + ":" + minute + ":" + second + ":" + (ampm == 0 ? "AM" : "PM") + "]";
-		out.println(timestamp + ": " + message);
-		if (fm != null)
+		FileManager.log(out, message);
+		if(fm != null)
 			fm.log(message);
 	}
 	
 	public String toString() {
 		return name + " v" + version;
+	}
+	
+	static class PrintWriterStream { //super hacky way of combining a PrintWriter and a PrintStream
+	
+		private PrintWriter w;
+		private PrintStream s;
+		
+		PrintWriterStream(PrintWriter w) { //support for PrintWriter
+			if(w == null)
+				throw new NullPointerException();
+			this.w = w;
+		}
+		
+		PrintWriterStream(PrintStream s) { //support for PrintStream
+			if(s == null)
+				throw new NullPointerException();
+			this.s = s;
+		}
+		
+		void println() {
+			if(w == null && s != null)
+				s.println();
+			else if(s == null && w != null)
+				w.println();
+			else
+				throw new NullPointerException("No valid output");
+		}
+		
+		void println(String x) {
+			if(w == null && s != null)
+				s.println(x);
+			else if(s == null && w != null)
+				w.println(x);
+			else
+				throw new NullPointerException("No valid output");
+		}
+		
+		public void flush() {
+			if(w == null && s != null)
+				s.flush();
+			else if(s == null && w != null)
+				w.flush();
+			else
+				throw new NullPointerException("No valid output");
+		}
 	}
 }
